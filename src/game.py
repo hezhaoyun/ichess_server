754bc6acc0d99a8029e963d9be9a040afdf9e259
hezhaoyun <hezhaoyun@outlook.com>
2024-11-01 20:19:59 +0800

    coding

--- src/game.py
+++ src/game.py
@@ -9,22 +9,21 @@ from src.server import running
 class Game:
 
     def __init__(self, pair):
-        
+
         self.players = pair
         self.player1 = pair[0]
         self.player2 = pair[1]
-        
+
         self.game_id = hash(self.player1) + hash(self.player2)
-        
-        # setting up the game board and Chessgame instance
+
         self.player_turn = 0
         self.this_turn_move_made = False
 
         self.board = chess.Board()
-        self.is_gameover = False
-        
+        self.is_game_over = False
+
         self.first_turn()
-        
+
     def first_turn(self):
 
         self.player_turn = 0  # index of player that is ought to make a move
@@ -32,17 +31,14 @@ class Game:
 
         self.new_board_state()
 
-        receivers = []
-        receivers.append(self.players[self.player_turn])
+        server.send_to([self.players[self.player_turn]], 'YOUR_MOVE')
 
-        server.send_to(receivers, "YOUR MOVE")
-
-        print(time.strftime("%H:%M") + " Waiting for a move from player, game ID = " + str(self.game_id))
+        print(f'{time.strftime('%H:%M')} Waiting for a move from player, game ID = {self.game_id}')
 
     def new_board_state(self):
         # This send_to out the board state to both players
-        server.send_to(self.players, str("\n" + str(self.board)))
-        print(time.strftime("%H:%M") + " GAME STATUS. ID = " + str(self.game_id))
+        server.send_to(self.players, f'\n{str(self.board)}')
+        print(f'{time.strftime('%H:%M')} GAME STATUS. ID = {self.game_id}')
         print(self.board)
 
     def after_move(self):
@@ -53,7 +49,7 @@ class Game:
                 if not (self.board.is_insufficient_material()):
                     # has the game been won
                     if not (self.board.is_checkmate()):
-                        if not (self.is_gameover):
+                        if not (self.is_game_over):
                             # WE GET TO PLAY, HURRAY
                             # sending board state to players
                             self.new_board_state()
@@ -64,37 +60,27 @@ class Game:
                             self.last_player = (self.last_player + 1) % 2
 
                             # message next player of his turn
-                            receivers = []
-                            receivers.append(self.players[self.player_turn])
-                            server.send_to(receivers, "YOUR MOVE")
+                            server.send_to([self.players[self.player_turn]], 'YOUR_MOVE')
 
                     else:
                         # player who made the last move won
-                        list = []
-                        list.append(self.players[self.player_turn])
-                        listtwo = []
-                        listtwo.append(self.players[self.last_player])
-                        self.declare_loser(listtwo, "Checkmate")
-                        self.declare_winner(list, "Checkmate")
+                        self.declare_loser([self.players[self.last_player]], 'Checkmate')
+                        self.declare_winner([self.players[self.player_turn]], 'Checkmate')
                 else:
                     # is a stalemate due to insufficient material
-                    self.draw("Insufficient material")
+                    self.draw('Insufficient material')
 
             else:
                 # is a stalemate
-                self.draw("Stalemate")
+                self.draw('Stalemate')
 
         else:
             # one of the players has disconnected
             # declaring winners
             if not (self.is_player_connected(self.player1)):
-                list = []
-                list.append(self.player2)
-                self.declare_winner(list, "Your opponent has disconnected")
+                self.declare_winner([self.player2], 'Your opponent has disconnected')
             else:
-                list = []
-                list.append(self.player1)
-                self.declare_winner(list, "Your opponent has disconnected")
+                self.declare_winner([self.player1], 'Your opponent has disconnected')
 
     def is_player_connected(self, player):
         if (player in running.players):
@@ -103,80 +89,65 @@ class Game:
             self.players.remove(player)
             return False
 
-    def declare_winner(self, player, reason):
-        server.send_to(player, "YOU WON!")
-        server.send_to(player, reason)
-        self.gameover()
+    def declare_winner(self, players, reason):
+        server.send_to(players, 'YOU WON!')
+        server.send_to(players, reason)
+        self.game_over()
 
-    def declare_loser(self, player, reason):
-        server.send_to(player, "YOU LOST!")
-        server.send_to(player, reason)
+    def declare_loser(self, players, reason):
+        server.send_to(players, 'YOU LOST!')
+        server.send_to(players, reason)
 
     def draw(self, reason):
-        server.send_to(self.players, "The game has ended in a draw.")
+        server.send_to(self.players, 'The game has ended in a draw.')
         server.send_to(self.players, reason)
-        self.gameover()
+        self.game_over()
 
     def player_disconnected(self, player):
         if (self.player1 == player):
-            list = []
-            list.append(self.player2)
-            self.declare_winner(list, "Your opponent has disconnected")
+            self.declare_winner([self.player2], 'Your opponent has disconnected')
         elif (self.player2 == player):
-            list = []
-            list.append(self.player1)
-            self.declare_winner(list, "Your opponent has disconnected")
+            self.declare_winner([self.player1], 'Your opponent has disconnected')
 
-    def gameover(self):
-        print("The game has ended. ID = " + str(self.game_id))
-        server.send_to(self.players, "GAMEOVER")
-        self.is_gameover = True
+    def game_over(self):
+        print(f'The game has ended. ID = {self.game_id}')
+        server.send_to(self.players, 'GAME_OVER')
+        self.is_game_over = True
 
         # remove the game from games list
         running.games.remove(self)
-        print(time.strftime("%H:%M") + " Removed a finished game from gamelist")
+        print(f'{time.strftime('%H:%M')} Removed a finished game from games')
 
         self.return_to_lobby_after_game()
 
     def return_to_lobby_after_game(self):
         # add the players to waiting list, maintain numbers and run matchmaking (since we're adding new players)
         for player in self.players:
-            # chess_server.ChessServer.waiting_list.append(player)
-            list = []
-            list.append(player)
             server.send_to(
-                list,
+                [player],
                 "You have been put in the matchmaking lobby again but won't be able to play the same opponent."
             )
 
         # SPECIAL COMMAND CODE to clients
         for player in self.players:
-            list_two = []
-            list_two.append(player)
-            server.send_to(list_two, "Type in MATCH to be matched again immediately.")
-            server.send_to(list_two, "WAITINGMATCH")
+            server.send_to([player], 'Type in MATCH to be matched again immediately.')
+            server.send_to([player], 'WAITING_MATCH')
 
     def get_message(self, message, player):
         # processes messages and return True/False depending if it was valid
-        if (str(message) == "FORFEIT"):
-            if (player == self.players[0]):
-                list = []
-                list.append(self.players[1])
-                self.declare_winner(list, "The opposite player has forfeited the game")
+        if (str(message) == 'FORFEIT'):
+            if (player == self.player1):
+                self.declare_winner([self.players2], 'The opposite player has forfeited the game')
                 return True
             else:
-                list = []
-                list.append(self.players[0])
-                self.declare_winner(list, "The opposite player has forfeited the game")
+                self.declare_winner([self.players1], 'The opposite player has forfeited the game')
                 return True
 
         elif (self.verify_move(message) == True):
             self.make_move(message)
             return True
         else:
-            list = []
-            list.append(player)
-            server.send_to(list, "Incorrect command, try again")
+            server.send_to([player], 'Incorrect command, try again')
             return False
 
     def verify_move(self, message):
@@ -185,9 +156,8 @@ class Game:
             if (chess.Move.from_uci(message) in self.board.legal_moves):
                 return True
             else:
-
                 return False
-        except (ValueError, IndexError) as wrongformatorillegalmove:
+        except (ValueError, IndexError) as wrong_format_or_illegal_move:
             return False
 
     def make_move(self, message):
--- src/server.py
+++ src/server.py
@@ -13,13 +13,13 @@ class running:
 def on_connect():
 
     # what happens when somebody connects
-    print(datetime.now().strftime("%H:%M") + " New connection made")
+    print(f'{datetime.now().strftime('%H:%M')} New connection made')
 
     running.players.append(request.sid)
 
     welcome()
 
-    send_to(running.waiting_players, "New player has connected and wants to play!")
+    send_to(running.waiting_players, 'New player has connected and wants to play!')
 
     running.waiting_players.insert(0, request.sid)
 
@@ -38,42 +38,39 @@ def on_disconnect():
     # Disconnected player was in a game, checking
     for game in running.games:
         if request.sid in game.players:
-            print("Player in a chess game has disconnected")
+            print('Player in a chess game has disconnected')
             game.player_disconnected(request.sid)
 
-    print("Connection Lost and handled by the server")
+    print('Connection Lost and handled by the server')
 
 
 def on_message(line):
 
     # we got something from a client
-    print(datetime.now().strftime("%H:%M ") + f"Received a message: {line}")
+    print(f'{datetime.now().strftime('%H:%M')} Received a message: {line}')
     
     if not line:
         # the message was somehow incorrect, sending special client side code to try again
-        send_to([request.sid], "TRYAGAIN")
+        send_to([request.sid], 'TRY_AGAIN')
 
     # is the client playing in a game
     player_in_game = False
 
     for game in running.games:
-
         if request.sid in game.players:
-
             player_in_game = True
 
-            if request.sid == game.players[game.player_turn] and not game.is_gameover:
-
+            if request.sid == game.players[game.player_turn] and not game.is_game_over:
                 if game.get_message(line, request.sid):
-                    print(datetime.now().strftime("%H:%M") + " Legit command was received and processed")
-                    # Engange the next turn function
+                    print(f'{datetime.now().strftime('%H:%M')} Legit command was received and processed')
+                    # Engage the next turn function
                     game.after_move()
 
                 else:
                     # the message was somehow incorrect, sending special client side code to try again
-                    send_to([request.sid], "TRYAGAIN")
+                    send_to([request.sid], 'TRY_AGAIN')
 
-    if not player_in_game and line == "MATCH" and request.sid not in running.waiting_players:
+    if not player_in_game and line == 'MATCH' and request.sid not in running.waiting_players:
         # the client is not playing in a game
         running.waiting_players.append(request.sid)
         match_making()
@@ -87,10 +84,10 @@ def send_to(sids, text):
 
 def welcome():
     # a bunch of on-login messages
-    send("Welcome to Chessroad.")
-    send("Server time: " + datetime.now().strftime("%H:%M"))
-    send("Connected players: " + str(len(running.players)))
-    send("Available players for matchmaking: " + str(len(running.waiting_players) + 1))
+    send('Welcome to Chessroad.')
+    send(f'Server time: {datetime.now().strftime('%H:%M')}')
+    send(f'Connected players: {len(running.players)}')
+    send(f'Available players for matchmaking: {len(running.waiting_players) + 1}')
 
 
 def match_making():
@@ -98,7 +95,7 @@ def match_making():
     # Is there enough players in waiting queue
     if (len(running.waiting_players) >= 2):
         
-        print(datetime.now().strftime("%H:%M") + " Matchmaking two players..")
+        print(f'{datetime.now().strftime('%H:%M')} Matchmaking two players..')
 
         # Creating a shortlist of matched players at the moment
         pair = []
@@ -106,13 +103,13 @@ def match_making():
         pair.append(running.waiting_players.pop(0))
 
         # Messaging players that a game has been found
-        send_to(pair, "Found a pair.. Connecting")
+        send_to(pair, 'Found a pair.. Connecting')
 
         make_game(pair)
 
     else:
         # Not enough players, you have to wait!
-        send("Please wait to be matched with another player")
+        send('Please wait to be matched with another player')
 
 
 def make_game(pair):
@@ -120,11 +117,11 @@ def make_game(pair):
     # Makes the game given list of any free two players
 
     # Sending over the command codes to initialize game modes on clients
-    send_to(pair, "GAMEMODE")
+    send_to(pair, 'GAME_MODE')
 
     # running the game
     import src.game as game
     the_game = game.Game(pair)
     running.games.append(the_game)
     
-    print(datetime.now().strftime("%H:%M") + " Hosted a game. ID = " + str(the_game.game_id))
+    print(f'{datetime.now().strftime('%H:%M')} Hosted a game. ID = {the_game.game_id}')
